c3a94e1b1520c1e71ff19d74fe364148202e2bef
Jhoterdan de Paula <jotaskt@gmail.com>
2014-05-25 16:07:43 -0300

    clean code

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
 	'twitterAuth' : {
 		'consumerKey' 		: 'bLIbmXTf9V8ScfxE8RWkqAYHW',
 		'consumerSecret' 	: 'l481ShaZ2nsdMSae9xDrkJGO7kQDgBr9vZWHvy2RV2sTs1LMQA',
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+		'callbackURL' 		: 'http://localhost:3030/auth/twitter/callback'
 	}
 
 };
